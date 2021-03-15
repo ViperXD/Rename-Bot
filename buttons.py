49ad61cd6581ce9f97d53aa27180c734a6c21e25
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-15 16:04:39 +0530

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,8 +1,8 @@
 from pyrogram.types import InlineKeyboardButton
 
 class Button(object):
-    START_BUTTONS = [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
-                        [InlineKeyboardButton('⚜ Help and Informations ⚜', callback_data='help')]]
+    START_BUTTONS = [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/VKPROJECTS'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/VKP_BOTS'),],
+                        [InlineKeyboardButton('⚙Help⚙', callback_data='help')]]
 
-    HELP_BUTTONS = [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/FayasNoushad'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/FayasChat'),],
+    HELP_BUTTONS = [[InlineKeyboardButton('⚙ Channel ⚙', url='https://telegram.me/VKPROJECTS'), InlineKeyboardButton('⚙ Group ⚙', url='https://telegram.me/VKP_BOTS'),],
                         [InlineKeyboardButton('⚜ Back to Home ⚜', callback_data='home')]]
