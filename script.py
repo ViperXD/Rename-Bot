4a72e40c9a34c589f6ac6a7f905f9dbbc161d7fd
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-15 13:20:53 +0530

    Initial commit

---
+++ script.py
@@ -0,0 +1,37 @@
+class script(object):
+    START_TEXT = """
+Hello {} , I'am a simple file or media rename bot with permanent thumbnail support.
+
+Made by @FayasNoushad
+"""
+    RENAME_403_ERR = "What Are You Doing? You are Banned"
+    DOWNLOAD_START = "<code>Downloading To My server Please Wait...</code>"
+    UPLOAD_START = "<code>Downloaded Successfully! Now I am Uploading to Telegram...</code>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me</b>"
+    SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
+    DEL_THUMB = "Thumbnail cleared succesfully!"
+    NO_THUMB = "No thumbnails found!"
+    CUSTOM_CAPTION_UL_FILE = "<b>Join :- @FayasNoushad</b>"
+    HELP_USER = """<b><u>More Help and Commands!</u></b>
+
+<b><u>Rename</u></b>
+➠ Send me any telegram file or media.
+➠ Choose appropriate option.
+
+<b><u>Set Thumbnail</u></b>
+➠ Send a photo to make it as custom thumbnail.
+
+<b><u>Deleting Thumbnail</u></b>
+➠ Send /delthumb to deleting thumbnail.
+
+<b><u>Show Thumbnail</u></b>
+➠ Send /showthumb for view current thumbnail.
+
+Made by @FayasNoushad
+"""
+    SAVED_RECVD_DOC_FILE = "<code>Downloaded Successfully! Now I am Uploading to Telegram...</code>"
+    SOMETHING_WRONG = "<code>Something Wrong. Try again.</code>"
+    FORCE_SUBSCRIBE_TEXT = "<code>Sorry Dear You Must Join My Updates Channel for using me 😌😉....</code>"
+    FREE_USER_LIMIT_Q_SZE = "Sorry Friend, Free users can only 1 request per {} minutes. Please try again after {} seconds later."
+    BANNED_USER_TEXT = "<code>You are Banned!</code>"
+    IFLONG_FILE_NAME = "Limits of telegram file or media name is {}."
