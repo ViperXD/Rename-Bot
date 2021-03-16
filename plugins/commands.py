e1870d46b2c1d449a114951c41e27d53e7201498
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-16 14:09:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -87,7 +87,7 @@ async def cus_name(bot, message):
 async def force_name(bot, message):
     await bot.send_message(
         message.reply_to_message.from_user.id,
-        "Enter new name for media with file type\n\nExample :- <code>sample.mkv | media</code>",
+        "Enter new name for media with file type\n\nExample :- <code>sample.mkv | Media Type</code>",
         reply_to_message_id=message.reply_to_message.message_id,
         reply_markup=ForceReply(True)
     )
